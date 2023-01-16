6dedf4c0ef2fb4e6695508d6a58aaa2e47e8ec67
Safak TURKELI <sfktrkl@gmail.com>
2023-01-17 02:43:19 +0300

    Helper

---
+++ 4/helper/src/lib.rs
@@ -0,0 +1,25 @@
+pub mod helper {
+    use rand::{thread_rng, Rng};
+    use std::time::Instant;
+
+    pub fn sort<T>(f: fn(&mut [T]) -> String, array: &mut [T]) {
+        let start = Instant::now();
+        let function_name = f(array);
+        let duration = start.elapsed();
+        println!("Time elapsed \"{}\":\n\t{:?}\n", function_name, duration);
+    }
+
+    const ARRAY_SIZE: usize = 1000;
+    pub fn create_array() -> [i32; ARRAY_SIZE] {
+        let mut array = [0i32; ARRAY_SIZE];
+        thread_rng().fill(&mut array[..]);
+        return array;
+    }
+
+    pub fn swap<T: Copy>(array: &mut [T], i: usize, j: usize) {
+        let temp: T;
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
